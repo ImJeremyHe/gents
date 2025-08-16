d8442e911818def75be858ea4512e7f0ed07b22f
Jeremy <yiliang.he@qq.com>
2025-08-16 11:10:00 +0800

    Add tests (#19)
    * Add tests
    
    * Fix

--- tests/src/serde_test/mod.rs
+++ tests/src/serde_test/mod.rs
@@ -3,18 +3,20 @@ use gents_derives::TS;
 use serde::{Deserialize, Serialize};
 use std::{fs, process::Command};
 
-const TS_TESTS_DIR: &str = "src/serde_test/data";
+const TS_TESTS_DIR: &str = "src/serde_test/ts";
+const JS_DIR: &str = "src/serde_test/ts";
+
 const CHECK_PATTERN: &str = r#"
-import fs from 'fs'
-import path from 'path'
+import * as fs from 'fs'
+import * as path from 'path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-import type { {type_name} } from '../data/{file_name}'
+import type { {type_name} } from './{file_name}'
 
-const jsonFile = path.resolve(__dirname, '../data/{file_name}.json')
+const jsonFile = path.resolve(__dirname, './{file_name}.json')
 const jsonData = JSON.parse(fs.readFileSync(jsonFile, 'utf8'))
 
 const typedData: {type_name} = jsonData
@@ -30,6 +32,51 @@ pub struct User {
     pub name: String,
 }
 
+#[derive(Serialize, Deserialize, TS)]
+#[ts(file_name = "test_enum.ts", rename_all = "camelCase", tag = "type")]
+pub enum TestEnum {
+    Variant1(User),
+    Variant2(User),
+}
+
+#[test]
+fn test_ts_rust_json_compatibility_enum() {
+    let file_name = "test_enum";
+    let type_name = "TestEnum";
+    let mut file_group = FileGroup::new();
+    file_group.add::<TestEnum>();
+    file_group.gen_files(TS_TESTS_DIR, false);
+
+    let test_enum = TestEnum::Variant1(User {
+        id: 42,
+        name: "Alice".to_string(),
+    });
+    let json = serde_json::to_string(&test_enum).unwrap();
+    fs::write(format!("{}/{}.json", JS_DIR, file_name), &json).unwrap();
+
+    let ts_file_content = CHECK_PATTERN
+        .replace("{file_name}", file_name)
+        .replace("{type_name}", type_name);
+    fs::write("src/serde_test/ts/check.ts", ts_file_content).unwrap();
+
+    let path = fs::canonicalize("src/serde_test/ts").unwrap();
+
+    let status = Command::new("npm")
+        .args(["run", "check"])
+        .current_dir(path)
+        .status()
+        .unwrap();
+    assert!(status.success(), "TypeScript failed to check types");
+
+    let gen_json = fs::read_to_string(format!("{}/{}.json", JS_DIR, file_name)).unwrap();
+    let test_enum_back: TestEnum = serde_json::from_str(&gen_json).unwrap();
+
+    match test_enum_back {
+        TestEnum::Variant1(_) => assert!(true),
+        _ => assert!(false),
+    }
+}
+
 #[test]
 fn test_ts_rust_json_compatibility() {
     let file_name = "user";
@@ -43,7 +90,7 @@ fn test_ts_rust_json_compatibility() {
         name: "Alice".to_string(),
     };
     let json = serde_json::to_string(&user).unwrap();
-    fs::write(format!("{}/{}.json", TS_TESTS_DIR, file_name), &json).unwrap();
+    fs::write(format!("{}/{}.json", JS_DIR, file_name), &json).unwrap();
 
     let ts_file_content = CHECK_PATTERN
         .replace("{file_name}", file_name)
@@ -52,14 +99,14 @@ fn test_ts_rust_json_compatibility() {
 
     let path = fs::canonicalize("src/serde_test/ts").unwrap();
 
-    let status = Command::new("npx")
-        .args(["tsx", "check.ts"])
+    let status = Command::new("npm")
+        .args(["run", "check"])
         .current_dir(path)
         .status()
         .unwrap();
     assert!(status.success(), "TypeScript failed to check types");
 
-    let gen_json = fs::read_to_string(format!("{}/{}.json", TS_TESTS_DIR, file_name)).unwrap();
+    let gen_json = fs::read_to_string(format!("{}/{}.json", JS_DIR, file_name)).unwrap();
     let user_back: User = serde_json::from_str(&gen_json).unwrap();
 
     assert_eq!(user_back.id, 42);
